ba22012320a975fc9ff282a1655602640b0270b5
jimanjang <67411280+jimanjang@users.noreply.github.com>
2021-02-01 23:37:17 +0900

    ummm ummm

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
 import "./styles.css";
 
-//rest
-export default function App() {
+const App = () => {
+  const [item, setItem] = useState(1);
+  const incrementItem = () => setItem(item + 1);
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h1>Hello {item}</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
-}
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
