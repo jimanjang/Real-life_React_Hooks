cf2ddc0f563f9157724a408af804d11bc2115e7f
jimanjang <67411280+jimanjang@users.noreply.github.com>
2021-02-04 20:29:40 +0900

    useInput two

--- src/index.js
+++ src/index.js
@@ -3,16 +3,26 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-const useInput = (initialValue) => {
+const useInput = (initialValue, validator) => {
   const [value, setValue] = useState(initialValue);
   const onChange = (event) => {
-    console.log(event.target);
+    const {
+      target: { value }
+    } = event;
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
   };
   return { value, onChange };
 };
 
 const App = () => {
-  const name = useInput("Mr.");
+  const maxLen = (value) => value.length <= 10;
+  const name = useInput("Mr.", maxLen);
   return (
     <div className="App">
       <h1>Hello </h1>
