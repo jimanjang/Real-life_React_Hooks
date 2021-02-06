8fbcaa172a01a3181582394a7be622e5c037b9cd
jimanjang <67411280+jimanjang@users.noreply.github.com>
2021-02-06 20:56:46 +0900

    Introduction to useEffect

--- src/index.js
+++ src/index.js
@@ -1,36 +1,16 @@
-import React, { useState, allTabs } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 
-const content = [
-  {
-    tab: "Section 1",
-    content: "I'm the content of the Section 1"
-  },
-  {
-    tab: "Section 2",
-    content: "I'm the content of the Section 2"
-  }
-];
-
-const useTabs = (initialTab, allTabs) => {
-  if (!allTabs || !Array.isArray(allTabs)) {
-    return;
-  }
-  const [currentIndex, setCurrentIndex] = useState(initialTab);
-  return {
-    currentItem: allTabs[currentIndex],
-    chageItem: setCurrentIndex
-  };
-};
-
 const App = () => {
-  const { currentItem, chageItem } = useTabs(0, content);
+  const sayHello = () => console.log("Hello");
+  const [number, setNumber] = useState(0);
+  const [aNumber, setAnumber] = useState(0);
+  useEffect(sayHello, []);
   return (
     <div className="App">
-      {content.map((section, index) => (
-        <button onClick={() => chageItem(index)}>{section.tab}</button>
-      ))}
-      <div>{currentItem.content}</div>
+      <div>Hi</div>
+      <button onClick={() => setNumber(number + 1)}>{number}</button>
+      <button onClick={() => setAnumber(aNumber + 1)}>{number}</button>
     </div>
   );
 };
